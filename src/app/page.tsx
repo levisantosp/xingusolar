41355b6d44f43a0fb3dd3dd2032ab5c491494778
levisantosp <contatowizard01@gmail.com>
2025-05-12 23:57:37 -0300

    algumas mudanças no tsx da pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image"
 
+const phrases: string[] = [
+        "Proporciona economia de até 95% na conta de luz",
+        "Proteção contra aumentos na tarifa",
+        "Valorização do imóvel em até 20%",
+        "Venda do imóvel até 30% mais rápida",
+        "Retorno do investimento em curto prazo",
+        "Durabilidade superior a 35 anos",
+        "Contribuição direta para um mundo sustentável",
+]
+
 export default function Page() {
         return (
                 <main>
@@ -25,64 +35,78 @@ export default function Page() {
                                 </div>
                         </div>
 
-                        <div className="instruction">
-                                <h1 className="instruction-title">
-                                        Como Funciona<br />
-                                        Geração de Energia Solar
-                                </h1>
-                                <div className="instruction-descs">
-                                        <div>
-                                                <Image
-                                                        src="/captacao.png"
-                                                        alt="Captação de Energia"
-                                                        width={80}
-                                                        height={80}
-                                                        className="instruction-img-1"
-                                                />
-                                                <p className="instruction-desc-1">Captação de Energia</p>
-                                        </div>
-                                        <div>
-                                                <Image
-                                                        src="/conversao.png"
-                                                        alt="Conversão e Distribuição"
-                                                        width={80}
-                                                        height={80}
-                                                        className="instruction-img-2"
-                                                />
-                                                <p className="instruction-desc-2">Conversão e Distribuição</p>
-                                        </div>
-                                        <div>
-                                                <Image
-                                                        src="/compartilhamento.png"
-                                                        alt="Compartilhamento de Energia"
-                                                        width={80}
-                                                        height={80}
-                                                        className="instruction-img-3"
-                                                />
-                                                <p className="instruction-desc-3">Compartilhamento de Energia</p>
-                                        </div>
-                                        <div>
-                                                <Image
-                                                        src="/seguranca.png"
-                                                        alt="Segurança e Monitoramento"
-                                                        width={80}
-                                                        height={80}
-                                                        className="instruction-img-4"
-                                                />
-                                                <p className="instruction-desc-4">Segurança e Monitoramento</p>
+                        <div className="instruction-how-container">
+                                <div className="instruction">
+                                        <h1 className="instruction-title">
+                                                Como Funciona<br />
+                                                Geração de Energia Solar
+                                        </h1>
+                                        <div className="instruction-descs">
+                                                <div>
+                                                        <Image
+                                                                src="/captacao.png"
+                                                                alt="Captação de Energia"
+                                                                width={80}
+                                                                height={80}
+                                                                className="instruction-img-1"
+                                                        />
+                                                        <p className="instruction-desc-1">Captação de Energia</p>
+                                                </div>
+                                                <div>
+                                                        <Image
+                                                                src="/conversao.png"
+                                                                alt="Conversão e Distribuição"
+                                                                width={80}
+                                                                height={80}
+                                                                className="instruction-img-2"
+                                                        />
+                                                        <p className="instruction-desc-2">Conversão e Distribuição</p>
+                                                </div>
+                                                <div>
+                                                        <Image
+                                                                src="/compartilhamento.png"
+                                                                alt="Compartilhamento de Energia"
+                                                                width={80}
+                                                                height={80}
+                                                                className="instruction-img-3"
+                                                        />
+                                                        <p className="instruction-desc-3">Compartilhamento de Energia</p>
+                                                </div>
+                                                <div>
+                                                        <Image
+                                                                src="/seguranca.png"
+                                                                alt="Segurança e Monitoramento"
+                                                                width={80}
+                                                                height={80}
+                                                                className="instruction-img-4"
+                                                        />
+                                                        <p className="instruction-desc-4">Segurança e Monitoramento</p>
+                                                </div>
                                         </div>
                                 </div>
-                        </div>
 
-                        <div>
-                                <h1>Por que escolher <br /> a Energia Solar?</h1>
-                                <h2>Proporciona economia de até 95% na conta de luz</h2>
-                                <h2>Proteção contra aumentos na Tarifa</h2>
-                                <h2>Valorização do imóvel em até 20%</h2>
-                                <h2>Venda do imóvel até 30% mais rápida</h2>
-                                <h2>Retorno do investimento em curto prazo</h2>
-                                <h2>Durabilidade superior a 35 anos</h2>
-                                <h2>Contribuição direta para um mundo sustentável</h2>
+                                <div className="how">
+                                        <h1 className="how-title">
+                                                Por que escolher<br />
+                                                a Energia Solar?
+                                        </h1>
+                                        <div>
+                                                {
+                                                        phrases.map((text, i) => (
+                                                                <div key={i} className="how-description">
+                                                                        <Image
+                                                                                src="/check.png"
+                                                                                alt="check"
+                                                                                width={20}
+                                                                                height={20}
+                                                                                className="how-img"
+                                                                        />
+                                                                        <p className="how-desc">{text}</p>
+                                                                </div>
+                                                        ))
+                                                }
+                                        </div>
+                                </div>
                         </div>
                 </main>
         )
